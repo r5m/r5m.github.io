b4e6c80646be5d208a14f1b6cc761a70630c84fb
mikhael <milikhin@gmail.com>
2014-08-28 13:10:52 +0700

    add ga

--- app/js/AppConfig.js
+++ app/js/AppConfig.js
@@ -255,13 +255,4 @@ function errorInitialization() {
 			( window.AppData.lang.indexOf('ru') > -1 || navigator.language.indexOf('ru')) ? "Ошибка инициализации" : "Initialization error"
 }
 
-//GOogLe Analytics
-if( location.toString().indexOf('r5m.github.io') > -1 && location.protocol.indexOf("https") >-1 ) {
-  setTimeout(function(){(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-39804194-5', 'auto');
-  ga('send', 'pageview');},100);
-}
