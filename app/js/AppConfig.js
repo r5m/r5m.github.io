9f0f1ff7eb444a4c23525f1048467da18c9e1b76
mikhael <milikhin@gmail.com>
2014-08-28 12:55:32 +0700

    add ga

--- app/js/AppConfig.js
+++ app/js/AppConfig.js
@@ -225,7 +225,7 @@ loadExchangeRates = function( json ) {
 					}
 				}
 			}
-			if(window.AppData.rates || localStorage.getItem( 'rates' ))
+			if(!!window.AppData.rates || !!localStorage.getItem( 'rates' ))
 				setupExchangeRates();
 			
 			require(["dojo/request"], function(request){
@@ -257,12 +257,11 @@ function errorInitialization() {
 
 //GOogLe Analytics
 if( location.toString().indexOf('r5m.github.io') > -1 && location.protocol.indexOf("https") >-1 ) {
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  setTimeout(function(){(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-39804194-5', 'auto');
-  ga('send', 'pageview');
-
+  ga('send', 'pageview');},100);
 }
