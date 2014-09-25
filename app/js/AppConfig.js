89e3fadb111aa1c45f63757b4f1bf0286438c6f2
mikhael <milikhin@gmail.com>
2014-09-25 11:05:05 +0000

    update to 0.9.8

--- app/js/AppConfig.js
+++ app/js/AppConfig.js
@@ -573,7 +573,6 @@ function confirmExit(){
 function loadGa(){
 	//GOogLe Analytics
 	if( location.toString().indexOf('r5m.github.io') > -1 && location.protocol.indexOf("https") >-1 ) {
-		 
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
