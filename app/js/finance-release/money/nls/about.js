ff9b8c286af1578931da60bae95e29dd041cbc0a
mikhael <milikhin@gmail.com>
2014-09-25 12:38:58 +0000

    add icons

--- app/js/finance-release/money/nls/about.js
+++ app/js/finance-release/money/nls/about.js
@@ -7,7 +7,8 @@ define("money/nls/about", {
 	    symba	: 'Note for Symbian (Nokia belle) users: ..Symbian, as you might know, also works on magic. So it\'s all about magic*magic = magic^2! ;-)',
 	    thanks	: 'Thanks to:',
 	    
-	    
+	    issueTitle	: 'If something went wrong',
+	    issue	: 'Report bug',
 	    latest	: 'Latest version already installed.',
 	    updating: 'Downloading updates...',
 	    ready	: 'Updates are ready to be installed.',
